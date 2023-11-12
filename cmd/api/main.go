084707840938244c01c57f24d16fdf16068ff841
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-12 13:42:08 +0100

    Refactor routers package to server package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"hichoma.chat.dev/internal/config"
 	"hichoma.chat.dev/internal/database"
-	"hichoma.chat.dev/internal/routers"
+	"hichoma.chat.dev/internal/server"
 )
 
 func main() {
 	config.InitializeAppConfig()
 	database.InitializeDatabase()
-	routers.InitializeRouter()
+	server.InitializeServer()
 }
