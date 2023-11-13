8c2e042aa85e7d7de9e5f2e6006903daed940ee8
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-13 23:10:18 +0100

    Add authentication middleware to protected routes

--- internal/server/init.go
+++ internal/server/init.go
@@ -4,6 +4,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"hichoma.chat.dev/internal/handlers"
+	middlewareFc "hichoma.chat.dev/internal/middleware"
 )
 
 func InitializeServer() {
@@ -14,6 +15,8 @@ func InitializeServer() {
 		AllowOrigins: []string{"*"},
 	}))
 	app.POST("/register", handlers.Register)
-	app.GET("/ws", handlers.WSEndpoint)
+	protectedRoutes := app.Group("/")
+	protectedRoutes.Use(middlewareFc.Authentificate)
+	protectedRoutes.GET("/ws", handlers.WSEndpoint)
 	app.Logger.Fatal(app.Start(":5000"))
 }
