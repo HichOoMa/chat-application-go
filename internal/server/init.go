85a33ddfc8ea1cf8c21db72c06589a3ffd0c9eab
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-18 22:07:49 +0100

    Fix middleware usage in server initialization

--- internal/server/init.go
+++ internal/server/init.go
@@ -10,13 +10,15 @@ import (
 func InitializeServer() {
 	app := echo.New()
 
-	// app.Use(middleware.Logger())
+	app.Use(middleware.Logger())
 	app.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 	}))
 	app.POST("/register", handlers.Register)
+
 	protectedRoutes := app.Group("/")
 	protectedRoutes.Use(middlewareFc.Authentificate)
 	protectedRoutes.GET("/ws", handlers.WSEndpoint)
+
 	app.Logger.Fatal(app.Start(":5000"))
 }
