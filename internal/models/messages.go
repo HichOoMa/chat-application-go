53e5f9d19fd3142bc776d7839a142ef4aa6cc033
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-27 20:39:54 +0100

    Add Message and Reactions struct to models

--- internal/models/messages.go
+++ internal/models/messages.go
@@ -1,4 +1,15 @@
 package models
 
 type Message struct {
+	SenderID       string      `json:"senderId" bson:"senderId"`
+	SenderUsername string      `json:"senderUsername" bson:"senderUsername"`
+	Content        string      `json:"content" bson:"content"`
+	Date           string      `json:"date" bson:"date"`
+	Reactions      []Reactions `json:"reactions" bson:"reactions"`
+}
+
+type Reactions struct {
+	Emoji  string   `json:"emoji" bson:"emoji"`
+	Number int      `json:"number" bson:"number"`
+	Users  []string `json:"users" bson:"users"`
 }
