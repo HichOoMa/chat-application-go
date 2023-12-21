1e6fd777b1070f48849d01224d6c8ec4a0f523f1
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-21 19:33:01 +0100

    message models

--- internal/models/messages.go
+++ internal/models/messages.go
@@ -1,15 +1,22 @@
 package models
 
 type Message struct {
-	SenderID       string      `json:"senderId" bson:"senderId"`
-	SenderUsername string      `json:"senderUsername" bson:"senderUsername"`
-	Content        string      `json:"content" bson:"content"`
-	Date           string      `json:"date" bson:"date"`
-	Reactions      []Reactions `json:"reactions" bson:"reactions"`
+	SenderID         string      `json:"senderId"          bson:"senderId"`
+	SenderUsername   string      `json:"senderUsername"    bson:"senderUsername"`
+	ReceiverID       string      `json:"receiver_id"       bson:"receiver_id"`
+	ReceiverUsername string      `json:"receiver_username" bson:"receiver_username"`
+	Content          string      `json:"content"           bson:"content"`
+	Date             string      `json:"date"              bson:"date"`
+	Reactions        []Reactions `json:"reactions"         bson:"reactions"`
 }
 
 type Reactions struct {
-	Emoji  string   `json:"emoji" bson:"emoji"`
+	Emoji  string   `json:"emoji"  bson:"emoji"`
 	Number int      `json:"number" bson:"number"`
-	Users  []string `json:"users" bson:"users"`
+	Users  []string `json:"users"  bson:"users"`
+}
+
+type WsMessage struct {
+	OppositeId string `json:"opposite_id" bson:"opposite_id"`
+	Content    string `json:"content"     bson:"content"`
 }
