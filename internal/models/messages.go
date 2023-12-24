80d0df21f622ef4bf84e780f36aabca63f2882e9
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-24 09:51:15 +0100

    Refactor Message struct in messages.go

--- internal/models/messages.go
+++ internal/models/messages.go
@@ -1,13 +1,16 @@
 package models
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Message struct {
-	SenderID         string      `json:"senderId"          bson:"senderId"`
-	SenderUsername   string      `json:"senderUsername"    bson:"senderUsername"`
-	ReceiverID       string      `json:"receiver_id"       bson:"receiver_id"`
-	ReceiverUsername string      `json:"receiver_username" bson:"receiver_username"`
-	Content          string      `json:"content"           bson:"content"`
-	Date             string      `json:"date"              bson:"date"`
-	Reactions        []Reactions `json:"reactions"         bson:"reactions"`
+	ID         primitive.ObjectID `json:"_id"         bson:"_id"`
+	SenderID   string             `json:"sender_id"   bson:"sender_id"`
+	ReceiverID string             `json:"receiver_id" bson:"receiver_id"`
+	Content    string             `json:"content"     bson:"content"`
+	Date       string             `json:"date"        bson:"date"`
+	Reactions  []Reactions        `json:"reactions"   bson:"reactions"`
 }
 
 type Reactions struct {
