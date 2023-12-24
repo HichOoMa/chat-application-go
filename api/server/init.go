e233184a8cf5d2787e7642c176801c1b601da458
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-24 09:51:03 +0100

    Add logger configuration and authentication endpoint

--- api/server/init.go
+++ api/server/init.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
 	"hichoma.chat.dev/api/handlers"
 	middlewareFc "hichoma.chat.dev/api/middleware"
 )
@@ -12,7 +13,9 @@ func InitializeServer() {
 
 	server := StartWS()
 
-	app.Use(middleware.Logger())
+	app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "${time_rfc3339} ${method} ${uri} ${status} ${latency_human}\n",
+	}))
 	app.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 	}))
@@ -20,9 +23,10 @@ func InitializeServer() {
 	app.POST("/login", handlers.Login)
 	app.GET("/checkToken", handlers.CheckToken)
 
-	protectedRoutes := app.Group("/")
+	protectedRoutes := app.Group("")
 	protectedRoutes.Use(middlewareFc.Authentificate)
 	protectedRoutes.GET("/ws", server.WSEndpoint)
+	protectedRoutes.GET("/auth", func(c echo.Context) error { return c.String(200, "valid") })
 
 	app.Logger.Fatal(app.Start(":5000"))
 }
