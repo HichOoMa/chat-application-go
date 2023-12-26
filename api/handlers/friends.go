4b506ebe323118c3ba7ff3eda38b9c23179ec2bb
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-26 23:56:45 +0100

    Add friends.go file with AddUserFriend and getUserFriendList handlers

---
+++ api/handlers/friends.go
@@ -0,0 +1,61 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"hichoma.chat.dev/internal/database"
+	"hichoma.chat.dev/internal/models"
+)
+
+type friendReq struct {
+	ID string `json:"_id"`
+}
+
+func AddUserFriend(ctx echo.Context) error {
+	userId := strings.Join(ctx.Request().Header.Values("user"), "")
+
+	bodyDTO := friendReq{}
+	ctx.Bind(&bodyDTO)
+	friendId, err := primitive.ObjectIDFromHex(bodyDTO.ID)
+	if err != nil {
+		return ctx.String(http.StatusBadRequest, "bad request")
+	}
+
+	friend := models.Friend{}
+	err = database.FindOneCollection("users", bson.M{"_id": friendId}, &friend)
+	if err != nil {
+		return ctx.String(http.StatusBadRequest, "friend id doesn't exist")
+	}
+	if userId == friend.ID.Hex() {
+		return ctx.String(http.StatusBadRequest, "cannot be friend with yourself")
+	}
+
+	userID, _ := primitive.ObjectIDFromHex(userId)
+	err = database.UpdateCollection(
+		"users",
+		bson.M{"_id": userID},
+		bson.M{"$push": bson.M{"friends": friend}},
+	)
+	if err != nil {
+		return ctx.String(http.StatusConflict, "can't add friend")
+	}
+
+	return ctx.String(http.StatusNoContent, "")
+}
+
+func getUserFriendList(ctx echo.Context) error {
+	userId := strings.Join(ctx.Request().Header.Values("user"), "")
+
+	user := models.User{}
+	filter := bson.M{"_id": userId}
+	err := database.FindOneCollection("users", filter, &user)
+	if err != nil {
+		return ctx.String(http.StatusConflict, "user collection not found")
+	}
+	return nil
+}
