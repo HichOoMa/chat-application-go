ebc388381d0d6189e9f7df2f566319103de8a3a1
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-26 08:38:33 +0100

    Fix user ID and date format in AddNewMessage function

--- api/handlers/message.go
+++ api/handlers/message.go
@@ -14,11 +14,12 @@ import (
 )
 
 func AddNewMessage(message *models.WsMessage, ctx echo.Context) *models.Message {
-	userId := strings.Join(ctx.Request().Header["user"], "")
-	date := time.Now().Format("2001-06-06 10:10:10")
+	userId := strings.Join(ctx.Request().Header.Values("user"), "")
+	date := time.Now().Format(time.RFC3339)
 	emptyReaction := []models.Reactions{}
 
 	msg := models.Message{
+		ID:         primitive.NewObjectID(),
 		SenderID:   userId,
 		ReceiverID: message.OppositeId,
 		Content:    message.Content,
@@ -37,7 +38,7 @@ func AddNewMessage(message *models.WsMessage, ctx echo.Context) *models.Message 
 }
 
 func GetUserMessages(ctx echo.Context) error {
-	userId := strings.Join(ctx.Request().Header["user"], "")
+	userId := strings.Join(ctx.Request().Header.Values("user"), "")
 	filter := bson.A{"$or", bson.A{bson.M{"sender_id": userId}, bson.M{"receiver_id": userId}}}
 
 	result := []models.Message{}
